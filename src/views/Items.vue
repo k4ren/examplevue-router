aa900be49f55e1a14c04346d08c4dfb5449b070b
k4ren <kkhakhis2@gmail.com>
2019-04-01 15:18:49 -0300

    router + params

--- src/views/Items.vue
+++ src/views/Items.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
-        <h1>Items:</h1>
+        <h1>Items: </h1>
+        <Item></Item>
     </div>
 </template>
+
+<script>
+import  Item  from "@/components/Item.vue";
+export default {
+    components: {
+        Item
+    }
+}
+</script>
